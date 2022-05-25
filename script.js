da1b09c6be9402b4adabd102723f187d76f902c3
Adedoyin Tioluwanimi <adedoyintioluwanimi@gmail.com>
2022-05-25 01:46:02 -0700

    My first Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var menu = document.getElementById('menu')
+var menuList = document.getElementById('menuList')
+
+menu.addEventListener('click', function(){
+   menuList.classList.toggle('navs')
+})
